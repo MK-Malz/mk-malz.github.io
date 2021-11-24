dde04263a151b1beaccad9f46ec82b88f9f10c2d
MK-Malz <klane.malte@gmail.com>
2021-11-24 17:07:01 +0100

    added basename to router in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/DoingGoodTogether">
       <App />
     </Router>
   </React.StrictMode>,
